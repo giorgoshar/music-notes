f0e45143dbe7103199946b3dfe6f8aa1bff331a0
giorgoshar <34591362+giorgoshar@users.noreply.github.com>
2021-04-20 01:21:06 +0300

    Refactoring the code to more OOP Adding Class Note

--- main.py
+++ main.py
@@ -1,40 +1,84 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=no-self-use
+
+# TODO: Next adding notes
+
+import os
+import sys
 import math
 import numpy as np
-import os
+import matplotlib.pyplot as plot
 
-volume  = 0.5
-seconds = 2
+class Note:
+    standardPitch = 440
+    def __init__(self, hz: float, sampleRate: int, bpm: float, volume: int):
+        self.hz         = hz
+        self.bpm        = bpm
+        self.sampleRate = sampleRate
+        self.volume     = volume
+
+    def sample(self, beat: float) -> list:
+        beat = beat * (60 / self.bpm)
+        sample  = self.freq(self.hz, beat)
+        attack  = list(self.attack(sample))
+        release = list(reversed(attack))
+
+        output = []
+        for frame, atk, rel in zip(sample, attack, release):
+            output.append((frame * atk * rel) * self.volume)
+        return output
+
+    def freq(self, hz: float, seconds: int) -> list:
+        step = (hz * 2 * math.pi) / self.sampleRate
+        sample = np.arange(0, self.sampleRate * seconds)
+        sample = map(lambda x: x * step, sample)
+        sample = map(math.sin, sample)
+        return list(sample)
+
+    def pure(self) -> list:
+        sample = self.freq(self.hz, 1)
+        return list(sample)
+
+    def sinwave(self) -> list:
+        sample = self.pure()
+        return sample[:int(self.sampleRate / self.hz)]
+
+    def attack(self, sample: list) -> list:
+        attack = map(lambda x: min(1, x / 1000), np.arange(0, len(sample)))
+        return attack
+
+    def release(self, sample: list) -> list:
+        release = map(lambda x: min(1, x / 1000), np.arange(0, len(sample)))
+        return release
+
+    @staticmethod
+    def semitone(n: int):
+        return 440 * (2 ** (1 / 12)) ** n
+
 sampleRate = 48000
-pitchStandard = 440
+bpm = 130
 
-def freq(hz, sec):
-    step   = (hz * 2 * math.pi) / sampleRate
-    sample = range(0, sampleRate * sec)
-    sample = map(lambda x: x * step, sample)
-    sample = map(math.sin, sample)
-    return list(sample)
+A4 = Note(440, sampleRate, bpm, 1)
+A5 = Note(Note.semitone(12), sampleRate, bpm, 1)
 
-def semitone(n):
-    return pitchStandard * (2 ** (1 / 12)) ** n
+plot.plot(A5.sinwave())
+plot.plot(A4.sinwave())
+# plot.plot(freq(440, 1))
+plot.ylabel('some numbers')
+plot.show()
+sys.exit()
 
-def note(n, sec):
-    return freq(semitone(n), sec)
-
-
-# wave = freq(440, 1) + freq(540, 2)
-# wave = [freq(pitchStandard + i * 100, 1) for i in range(0, 10)]
+notes = [
+    A4.pure() + A5.pure(),
+]
 
 wave = []
-# for i in range(0, 10):
-    # wave += note(2 * i, 1)
+for note in notes:
+    wave += note
+wave = np.array(list(wave)).astype(np.float32).tobytes()
 
-wave = note(0, 1) + note(2, 1) + note(4, 1) + note(5, 1) + note(7, 1) + note(9, 1) + note(11, 1) + note(12, 1)
-
-wave = map(lambda x: x * volume, wave) 
-wave = list(wave)
-wave = np.array(wave).astype(np.float32).tobytes()
-
-with open('output.bin', 'wb') as output:
+outputfile = 'output.bin'
+with open(outputfile, 'wb') as output:
     output.write(wave)
 
-os.system(f'ffplay.exe -showmode 1 -f f32le -ar {sampleRate} output.bin')
+os.system(f'ffplay.exe -stats -showmode 1 -f f32le -ar {sampleRate} {outputfile}')
