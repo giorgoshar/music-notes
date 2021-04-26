fd3cc6a1d92566b4e61631a93a09f87bf5c0abe1
giorgoshar <34591362+giorgoshar@users.noreply.github.com>
2021-04-26 19:05:45 +0300

    Adding simpleaudio library

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 import sys
 import math
 import numpy
+import simpleaudio
 
 from Oscillators import *
 from Envelop     import *
 from Utils       import *
 
-sampleRate = 44000
+sampleRate = 44100
 seconds    = 1
 bpm        = 130
 
@@ -31,30 +32,50 @@ Oscillators = [
 # plot.title('sine wave')
 # plot.show()
 
+class Amplifier:
+    def __init__(self, amplitude=1):
+        self.amplitude = amplitude
+
+class Wave(SinOscillator):
+    def __init__(self, freq, amp, sampleRate, seconds):
+        SinOscillator.__init__(self, freq, amp, sampleRate * seconds)
+        self.modules = []
+
+    def add_modulation(self, SampleModulation):
+        # https://www.cs.cmu.edu/~music/icm-online/readings/fm-synthesis/index.html
+        self.modules.append(SampleModulation)
+        output = []
+        for (sample, mod) in zip(self.sample, SampleModulation.sample):
+            output.append((sample + mod) * 0.5)
+        self.sample = output
+
+
 env = Envelop(0.075, 0.25, 0.25, 0.25, sampleRate)
 
-# A4_2  = Wave(444, 1, sampleRate, 10)
 A4 = Wave(444, 1, sampleRate, 1)
+A5 = Wave(888, 1, sampleRate, 1)
 original_wave = list(A4.sample)
 
-Modulator1 = list(Wave(888, 1, sampleRate, 1).sample)
-Modulator2 = list(Wave(1320, 1, sampleRate, seconds).sample)
-Modulator3 = list(TriangleOscillator(440, 1, sampleRate * seconds).sample)
+Modulator1 = Wave(888, 1, sampleRate, 1)
+Modulator2 = Wave(10,  1, sampleRate, 1)
+Modulator3 = TriangleOscillator(440, 1, sampleRate * seconds)
 
 # A4.add_modulation(Modulator1)
 # A4.add_modulation(Modulator2)
-A4.add_modulation(Modulator3)
+# A4.add_modulation(Modulator3)
 
-Utils.plot('Sinusoids',[
-    {'title': f'Wave {A4.freq} {A4.seconds}', 'array': original_wave,  'color': 'red'},
-    {'title': f'Modulation 1',  'array':  Modulator1,  'color': 'red'},
-    # {'title': 'Modulation 2', 'array': Modulator2,  'color': 'red'},
-    # {'title': 'Modulation 3', 'array': Modulator3,  'color': 'red'},
-    {'title': 'Modulation Output', 'array': A4.sample,  'color': 'red'},
-])
+# Utils.plot('Sinusoids',[
+#     {'title': f'Wave {A4.freq} {A4.seconds}', 'array': original_wave[:1000],  'color': 'red', 'obj': A4 },
+#     {'title': f'Modulation ({Modulator1.freq}Hz)',  'array':  Modulator1.sample[:1000],  'color': 'red'},
+#     {'title': 'Modulation 2', 'array': Modulator2.sample[:1000],  'color': 'red'},
+#     {'title': 'Modulation 3', 'array': Modulator3.sample[:1000],  'color': 'red'},
+#     {'title': 'Modulation Output', 'array': A4.sample[:1000],  'color': 'red'},
+# ])
 
 notes = [
     A4.sample,
+    A5.sample,
+    TriangleOscillator(10, 1, sampleRate * seconds).sample
 ]
 
 wave = []
@@ -62,8 +83,5 @@ for note in notes:
     wave += note
 wave = numpy.array(list(wave)).astype(numpy.float32).tobytes()
 
-outputfile = 'output.bin'
-with open(outputfile, 'wb') as output:
-    output.write(wave)
-
-os.system(f'ffplay.exe -stats -showmode 1 -f f32le -ar {sampleRate} {outputfile}')
+play_obj = simpleaudio.play_buffer(wave, 1, 4, 44100)
+play_obj.wait_done()
